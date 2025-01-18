f4572935bddb898966007b2ac11a150df89ecc11
Gurasuraisu <dakirb@proton.me>
2025-01-18 20:25:01 +0000

    Update index.js
    Signed-off-by: Gurasuraisu <dakirb@proton.me>

--- js/index.js
+++ js/index.js
@@ -46,12 +46,12 @@ consoleGreeting()
 
 const secondsSwitch = document.getElementById('seconds-switch');
 const appUsage = {};
+const weatherSwitch = document.getElementById('weather-switch');
 
 let showSeconds = localStorage.getItem('showSeconds') !== 'false'; // defaults to true
 let showWeather = localStorage.getItem('showWeather') !== 'false'; // defaults to true
 
 secondsSwitch.checked = showSeconds;
-weatherSwitch.checked = showWeather;
 
 // IndexedDB setup for video storage
 const dbName = 'WallpaperDB';
