814d5dd3796cc5a9ba3d872c9e9a0bdcb633e79a
Gurasuraisu <dakirb@proton.me>
2025-01-19 16:17:48 +0000

    Update index.js
    Signed-off-by: Gurasuraisu <dakirb@proton.me>

--- js/index.js
+++ js/index.js
@@ -1984,8 +1984,7 @@ function setupDrawerInteractions() {
     const flickVelocityThreshold = 0.4;
     const dockThreshold = -25; // Threshold for dock appearance
     const openThreshold = -50;
-    const drawerPill = document.querySelector('.drawer-pill');
-    const drawerHandle = document.querySelector('.drawer-pill');
+    const drawerHandle = document.querySelector('.drawer-handle');
     
     // Create dock element
     const dock = document.createElement('div');
