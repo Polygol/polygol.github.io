08fbb4ed4daeffe1a25d77e3faff59f93317e220
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-09-14 12:35:23 +0100

    Update lang-app.js

--- assets/gurapp/lang-app.js
+++ assets/gurapp/lang-app.js
@@ -477,3 +477,14 @@ const LANG_ZH_APP = {
     INSTALLED: "已安装",
     DEVELOPER: "开发者"
 };
+
+if (window.Gurasuraisu && Gurasuraisu._registerGlobalLangPack) {
+    Gurasuraisu._registerGlobalLangPack({
+        EN: LANG_EN_APP,
+        JP: LANG_JP_APP,
+        DE: LANG_DE_APP,
+        ES: LANG_ES_APP,
+        KO: LANG_KO_APP,
+        ZH: LANG_ZH_APP
+    });
+}
