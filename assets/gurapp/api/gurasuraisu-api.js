595e3b35bf6c231fc7babe97a0fcf95a87ac6eae
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-10-26 18:02:52 +0000

    Update gurasuraisu-api.js

--- assets/gurapp/api/gurasuraisu-api.js
+++ assets/gurapp/api/gurasuraisu-api.js
@@ -236,7 +236,7 @@ const _myActiveActivities = new Set(); // NEW: Tracks this app's active activiti
         }
 
         html.gurasuraisu-glass-disabled {
-            --edge-refraction-filter: grayscale(0); /* Use nonsense */
+            --edge-refraction-filter: blur(5px); /* Frosted glass appearance */
         }
 
         :root.standalone {
