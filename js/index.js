ca3afcb58c6e0ad6c6621104ad01014f8b453d29
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-11-19 18:11:21 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3993,7 +3993,7 @@ async function initializeAiAssistant() {
         }];
         
         const model = genAI.getGenerativeModel({
-            model: "gemini-2.5-flash-lite-preview-06-17",
+            model: "gemini-2.5-flash-lite",
             tools: tools,
             systemInstruction: systemInstruction,
 	    generationConfig: {
