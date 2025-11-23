325ed3f7c993b8b294876214a59236aa3ff13cc5
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-11-23 19:00:30 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ const ASSETS_TO_CACHE = [
   '/assets/appicon/settings.png',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdn.jsdelivr.net/npm/suncalc@1.9.0/suncalc.min.js',
-  'https://cdn.jsdelivr.net/npm/trystero@0.22.0/src/index.min.js',
+  'https://esm.sh/trystero@0.15.1/torrent',
   'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
   'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap',
   'https://cdn.jsdelivr.net/gh/lauridskern/open-runde@main/src/web/OpenRunde-Regular.woff2',
