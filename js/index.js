97350c7b496cee0329c62e816b735d4595574f82
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2026-01-19 19:18:56 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8839,7 +8839,7 @@ function renderSwitcherCards(container) {
         // Edit Button
         const editBtn = document.createElement('button');
         editBtn.className = 'switcher-edit-btn';
-        editBtn.innerHTML = '<span class="material-symbols-rounded">settings</span> Edit';
+        editBtn.innerHTML = 'Edit';
         editBtn.onclick = (e) => {
             e.stopPropagation();
             openWallpaperEditMenu(index);
